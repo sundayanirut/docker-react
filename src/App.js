abf7b069923729dc8ded344f8bdf88a9a5a33bca
sundayanirut <sundayanirut@gmail.com>
2020-03-30 19:47:02 +0000

    changed app test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed!!
+          Hi SUNDAYYYY!!
         </p>
         <a
           className="App-link"
